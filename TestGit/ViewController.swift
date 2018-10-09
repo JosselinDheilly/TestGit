33ac48840e8889f2306f36bcc373755cabbf3fef
Josselin Dheilly Devinnova <josselin_devinnova@macbook-pro-de-josselin-199.local>
2018-10-09 18:57:14 +0200

    saurais du modifier l'autre

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -15,5 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Test")
     }
+    
+    func sayHello() {
+        
+        
+    }
 }
 
